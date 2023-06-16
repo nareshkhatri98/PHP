b9b63d64f98dbd949aad06a0532ae9c103b34d98
Naresh Khatri <111528185+nareshtestgithub@users.noreply.github.com>
2023-06-16 08:37:54 +0545

    add note

--- oop in php/abstraction.php
+++ oop in php/abstraction.php
@@ -14,4 +14,6 @@ abstract class A{
  $obj = new B;
  $obj->any();
 
+
+  // the child class have any abstract class having abstract method can have argument or parameters in comparsion to parent abstract functiion or method..
  ?>
